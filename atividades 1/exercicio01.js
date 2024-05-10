6bd32e254dadc1f524581b7b1eeaba07b860c58d
Iohannes Paulus <126628609+JaumZiiZ@users.noreply.github.com>
2024-05-10 11:57:05 -0300

    Add files via upload

---
+++ atividades 1/exercicio01.js
@@ -0,0 +1,45 @@
+/*
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    num +=1;
+} 
+*/
+
+
+/*
+let num2 = 10 
+while (num2 >= 0) {
+    console.log(num2)
+    num2 -=1;
+}
+*/
+
+/*
+let numero = 
+parseInt(prompt("Digite um número para a contagem regressiva:"));
+
+if (isNaN(numero)) {
+    console.log("Por favor, digite um numero valido.")
+} else {
+    while (numero >0) {
+        console.log(numero);
+        numero -=1 
+    }
+}
+*/
+
+/*
+let numero = 
+parseInt(prompt("Digite um número para a contagem regressiva:"));
+
+if (isNaN(numero)) {
+    console.log("Por favor, digite um numero valido.")
+} else {
+    let contador = 0
+    while (contador <= numero) {
+        console.log(contador);
+        contador +=1 
+    }
+}
+*/
